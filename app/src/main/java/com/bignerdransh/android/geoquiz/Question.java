1749de8d092c14e2c6eaaa356187c95396dbcbb1
冯雪飞 <yomacangye@outlook.com>
2016-08-10 19:52:09 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdransh/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdransh.android.geoquiz;
+
+/**
+ * Created by fxf on 2016/8/5.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
